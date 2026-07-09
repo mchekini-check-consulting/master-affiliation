199d68ce0643a9bbeb04a881d68053c76b07578d
mchekini <me.chekini@gmail.com>
2026-07-09 15:16:06 +0200

    v1 - projet site A

--- site-a/astro.config.mjs
+++ site-a/astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-// TODO: remplacer par le vrai domaine du site quand la niche sera choisie.
 export default defineConfig({
-  site: 'https://site-a.example.com',
+  site: 'https://aquascaping-facile.fr',
   integrations: [sitemap()],
 });
